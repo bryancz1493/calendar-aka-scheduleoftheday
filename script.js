23bcf45ddbbb8bac0ced1bcd9bed347650f6b6a9
bryancz1493 <bryancz1493@gmail.com>
2023-10-24 22:31:13 -0700

    Update parameters (0900-1700) to show 'past', 'present', 'future' correctly

--- script.js
+++ script.js
@@ -6,13 +6,6 @@ var today = dayjs();
 var saveButton = document.getElementsByClassName(".btn saveBtn");
 $('#currentTime').text(today.format('h:mm:ss A' ));
 $('#currentDay').text(today.format('MMM D, YYYY'));
-
-// Create function to show live time 
-// function liveTime() {
-    //     var updateLiveTime
-    //     updateLiveTime();
-    //     setInterval(updateLiveTime, 1000);
-    // }
     
     // TODO: Add a listener for click events on the save button. This code should
     // use the id in the containing time-block as a key to save the user input in
@@ -24,7 +17,7 @@ $('#currentDay').text(today.format('MMM D, YYYY'));
     function clickToSave(hour) {
         hour.preventDefault();
         var saveButton = document.getElementsByClassName('fas fa-save');
-        var inputInTextArea = document.getElementsByClassName(".col-8 col-md-10 description");
+        var inputInTextArea = document.getElementsByClassName(".col-8 col-md-10 description").value;
         saveButton.addEventListener('save', clickToSave);
 
         window.localStorage.getItem('hour', 'inputInTextArea')
@@ -33,31 +26,34 @@ $('#currentDay').text(today.format('MMM D, YYYY'));
     }
     
     // Declare work time (9am-5pm)
-    for (let hour = 9; hour <= 17; hour++) {
-    var workTime = dayjs().hour();
-    
-    let timeBlock = 'future';
-    if (hour < workTime) {
-        timeBlock = 'past';
-    } else if (hour === workTime) {
-        timeBlock = 'present';
-    }
-
-    // var timeBlock = document.getElementById('#hour-9');
-}
-
-
-$(function () {
     // TODO: Add code to apply the past, present, or future class to each time
     // block by comparing the id to the current hour. HINTS: How can the id
     // attribute of each time-block be used to conditionally add or remove the
     // past, present, and future classes? How can Day.js be used to get the
     // current hour in 24-hour time?
     //
+    for (let hour = 9; hour <= 17; hour++) {
+    var workTime = dayjs().hour();
+    var timeBlock = $('#hour-9, #hour-10, #hour-11, #hour-12, #hour-13, #hour-14, #hour-15, #hour-16, #hour-17');
+    
+    if (hour < workTime) {
+        timeBlock.addClass('past');
+    } else if (hour === workTime) {
+        timeBlock.addClass('present');
+    } else {
+        timeBlock.addClass('future');
+    }
+}
 
-    // TODO: Add code to get any user input that was saved in localStorage and set
-    // the values of the corresponding textarea elements. HINT: How can the id
-    // attribute of each time-block be used to do this?
-    //
+// TODO: Add code to get any user input that was saved in localStorage and set
+// the values of the corresponding textarea elements. HINT: How can the id
+// attribute of each time-block be used to do this?
+//
+// function userInput() {
+//     var input = 
+// }
+
+$(function () {
+
 
   });
