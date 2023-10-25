6c46829c6b4b909bbcaf2524b8732d821f29d4b5
bryancz1493 <bryancz1493@gmail.com>
2023-10-24 22:11:29 -0700

    Set local storage for input in text area (it is not working right now); trying to figure out whats wrong in my code

--- script.js
+++ script.js
@@ -3,34 +3,51 @@
 // in the html.
 // TODO: Add code to display the current date in the header of the page.
 var today = dayjs();
-var workTime = dayjs().hour();
-var saveButton = document.getElementsByClassName(".btn saveBtn col-2 col-md-1");
+var saveButton = document.getElementsByClassName(".btn saveBtn");
 $('#currentTime').text(today.format('h:mm:ss A' ));
 $('#currentDay').text(today.format('MMM D, YYYY'));
 
 // Create function to show live time 
 // function liveTime() {
-//     var updateLiveTime
-//     updateLiveTime();
-//     setInterval(updateLiveTime, 1000);
-// }
+    //     var updateLiveTime
+    //     updateLiveTime();
+    //     setInterval(updateLiveTime, 1000);
+    // }
+    
+    // TODO: Add a listener for click events on the save button. This code should
+    // use the id in the containing time-block as a key to save the user input in
+    // local storage. HINT: What does `this` reference in the click listener
+    // function? How can DOM traversal be used to get the "hour-x" id of the
+    // time-block containing the button that was clicked? How might the id be
+    // useful when saving the description in local storage?
+    //
+    function clickToSave(hour) {
+        hour.preventDefault();
+        var saveButton = document.getElementsByClassName('fas fa-save');
+        var inputInTextArea = document.getElementsByClassName(".col-8 col-md-10 description");
+        saveButton.addEventListener('save', clickToSave);
 
-// TODO: Add a listener for click events on the save button. This code should
-// use the id in the containing time-block as a key to save the user input in
-// local storage. HINT: What does `this` reference in the click listener
-// function? How can DOM traversal be used to get the "hour-x" id of the
-// time-block containing the button that was clicked? How might the id be
-// useful when saving the description in local storage?
-//
-function clickToSave(event) {
-    event.preventDefault();
-    var saveButton = document.getElementsByClassName('fas fa-save');
-    saveButton.addEventListener('save', clickToSave);
+        window.localStorage.getItem('hour', 'inputInTextArea')
+        window.localStorage.setItem('hour', 'clickToSave');
+        // document.getElementById('hour-9').innerHTML = window.localStorage.clickToSave; 
+    }
+    
+    // Declare work time (9am-5pm)
+    for (let hour = 9; hour <= 17; hour++) {
+    var workTime = dayjs().hour();
+    
+    let timeBlock = 'future';
+    if (hour < workTime) {
+        timeBlock = 'past';
+    } else if (hour === workTime) {
+        timeBlock = 'present';
+    }
+
+    // var timeBlock = document.getElementById('#hour-9');
 }
 
-$(function () {
-    // document.getElementById('hour-9') = ;
 
+$(function () {
     // TODO: Add code to apply the past, present, or future class to each time
     // block by comparing the id to the current hour. HINTS: How can the id
     // attribute of each time-block be used to conditionally add or remove the
